760a34551b8efc34af22a81b661c48950d30c4dd
arun <arun@medzdigital.com>
2019-04-07 23:14:33 +0530

    added sticky header

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-    $('.bxslider').bxSlider();
+var landingHeight = $('.rq-landing').height();
+var yourNavigation = $(".rq-header");
+stickyDiv = "sticky";
+yourHeader = landingHeight;
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > yourHeader) {
+        yourNavigation.addClass(stickyDiv);
+        yourNavigation.addClass('sticky');
+        $('.rq-header .rq-logo').children('img').attr('src', 'assets/images/rq-logo-secondary.svg');
+        // $('.desktop-menu ul li.contact-info').css('display', 'inline-block');
+    } else {
+        yourNavigation.removeClass(stickyDiv);
+        yourNavigation.removeClass('sticky');
+        $('.rq-header .rq-logo').children('img').attr('src', 'assets/images/rq-logo-primary.svg');
+        // $('.desktop-menu ul li.contact-info').css('display', 'none');
+    }
+});
+
+$('.nav-list li').click(function () {
+   $('.nav-list li').removeClass('active');
+    $(this).addClass('active');
 });
